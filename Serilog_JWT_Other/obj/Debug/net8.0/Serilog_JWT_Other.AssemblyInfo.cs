fb077c0aedf1f363df98063da9604b4a97cf0f23
LynnKhine <linnthantkhine2017@gmail.com>
2025-01-04 22:34:11 +0800

    File path added

--- Serilog_JWT_Other/obj/Debug/net8.0/Serilog_JWT_Other.AssemblyInfo.cs
+++ Serilog_JWT_Other/obj/Debug/net8.0/Serilog_JWT_Other.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Serilog_JWT_Other")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d915813d94f7adb00b86617ad9fb4afdfcc1edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b4082625bef00d0a3e96e8fbeb84e527c364e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Serilog_JWT_Other")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Serilog_JWT_Other")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
